41febf4caef4689174a548efd47db3a78806b352
Amosh07 <amoshhamal7@gmail.com>
2025-01-03 20:56:39 +0545

    Add Transactions model

---
+++ DataAccess/Services/TransactionsService.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Interface;
+using DataModel.Abstraction;
+using DataModel.Model;
+
+namespace DataAccess.Services
+{
+    public class TransactionsService : UserBase, ITransactionsInterface
+    {
+        public bool AddTransactions(Transactions transactions)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Money/MauiProgram.cs
+++ Money/MauiProgram.cs
@@ -22,6 +22,7 @@ namespace Money
 #if DEBUG
             //Service inject//
             builder.Services.AddScoped<IUserInterface,UserService>();
+            builder.Services.AddScoped<ITransactionsInterface, TransactionsService>();
             builder.Services.AddBlazorWebViewDeveloperTools();
     		builder.Logging.AddDebug();
 #endif
